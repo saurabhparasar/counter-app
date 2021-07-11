da234f66dc77822d7202cca06781c05ea8db4441
saurabh parasar <saurabhparasar3010@gmail.com>
2021-07-12 01:45:53 +0530

    removing local state

--- src/components/Counters.jsx
+++ src/components/Counters.jsx
@@ -10,6 +10,10 @@ class Counters extends Component {
       { id: 4, value: 0 },
     ],
   };
+  handleIncrement = (counter) => {
+    console.log(counter);
+  };
+
   handleDelete = (counterId) => {
     const counters = this.state.counters.filter((c) => c.id !== counterId);
     this.setState({ counters });
@@ -35,6 +39,7 @@ class Counters extends Component {
           <Counter
             key={counter.id}
             onDelete={this.handleDelete}
+            onIncrement={this.handleIncrement}
             counter={counter}
           />
         ))}
